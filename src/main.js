823e6d446bf8cfe581161712541ec9638432352c
Traderxue <falethe@gmail.com>
2023-12-23 10:34:19 +0800

    init

--- src/main.js
+++ src/main.js
@@ -5,8 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import 'vant/lib/index.css';
+import vant from "vant"
 
 const app = createApp(App)
+
+app.use(vant)
 
 app.use(createPinia())
 app.use(router)
